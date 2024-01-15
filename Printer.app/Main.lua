9bc6be88145b2180ebfedda6eddc722e2061b05b
ke46138 <ke46138@gmail.com>
2024-01-15 11:23:01 +0300

    УРРА ОБНОВА! Теперь можно печатать .txt файлы

--- Printer.app/Main.lua
+++ Printer.app/Main.lua
@@ -1,9 +1,11 @@
 local GUI = require("GUI")
 local system = require("System")
+local filesystem = require("Filesystem")
 local component = require("component")
 local localization = system.getCurrentScriptLocalization()
 title = "Lol"
 documentString = "Lol"
+flag = false
 if not component.isAvailable("openprinter") then
   GUI.alert(localization.notAvailablePrinter)
   return
@@ -35,18 +37,39 @@ stringD.onInputFinished = function()
   documentString = stringD.text
 end
 
-local printB = window:addChild(GUI.button(2, 12, 25, 3, 0xFFFFFF, 0x555555, 0x880000, 0xFFFFFF, localization.printButton))
+local printB = window:addChild(GUI.button(2, 12, 25, 3, 0xFFFFFF, 0x555555, 0x3C3C3C, 0xFFFFFF, localization.printButton))
 printB.onTouch = function()
   --openprinter.printTag("String")
   openprinter.setTitle(title)
-  openprinter.writeln(documentString)
+  if flag == false then
+    openprinter.writeln(documentString)
+  end
   openprinter.print()
+  flag = false
 end
 
 window:addChild(GUI.text(29, 4, 0x000000, localization.inkBlack))
 window:addChild(GUI.text(29, 5, 0x000000, localization.inkColor))
 window:addChild(GUI.text(46, 4, 0x000000, openprinter.getBlackInkLevel()))
 window:addChild(GUI.text(46, 5, 0x000000, openprinter.getColorInkLevel()))
+
+local filesystemChooser = window:addChild(GUI.filesystemChooser(27, 8, 33, 3, 0xE1E1E1, 0x888888, 0x3C3C3C, 0x888888, nil, localization.open, localization.cancel, localization.choose, "/"))
+filesystemChooser:setMode(GUI.IO_MODE_OPEN, GUI.IO_MODE_FILE)
+filesystemChooser.onSubmit = function(path)
+  --GUI.alert("File \"" .. path .. "\" was selected")
+  --GUI.alert(system.call(sugoma, path))
+    --local handle = filesystem.open(pathArg, "r")
+  local s1 = filesystem.readLines(path)
+   for i = 1, #s1, 1 do
+      pidor = s1[i]
+      openprinter.writeln(pidor)
+   end
+  --handle:close()
+  --openprinter.writeln(s1)
+  
+  flag = true
+end
+
 
 window.onResize = function(newWidth, newHeight)
   window.backgroundPanel.width, window.backgroundPanel.height = newWidth, newHeight
